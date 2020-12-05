4ba1709f44959d76f28d561a0331a05e1b3572a4
Mollie Goforth <molliegoforth818@gmail.com>
2020-12-05 14:44:08 -0600

    navbar deleted, prof_exp url working

--- prof_exps/views.py
+++ prof_exps/views.py
@@ -5,6 +5,6 @@ def home(request):
     prof_exps = Prof_Exp.objects
     return render(request, 'prof_exps/home.html', {'prof_exps' : prof_exps})
 
-def detail(request, prof_exp_id): 
-    prof_exp_detail = get_object_or_404(Prof_Exp, pk=prof_exp_id)
-    return render(request, 'prof_exps/detail.html', {'prof_exp': prof_exp_detail})     
+def prof_exp_detail(request): 
+    prof_exp_detail = Prof_Exp.objects
+    return render(request, 'prof_exps/prof_exp_detail.html', {'prof_exps': prof_exp_detail})     
