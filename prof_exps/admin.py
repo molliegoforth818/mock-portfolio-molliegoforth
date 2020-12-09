9fcd4abdd6c1ae90c0ddf8340a12ff816037d9e9
Mollie Goforth <molliegoforth818@gmail.com>
2020-12-08 22:54:06 -0600

    new models added for education and about me page

--- prof_exps/admin.py
+++ prof_exps/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Prof_Exp
+from .models import Prof_Exp,Education, About_Me
 
 admin.site.register(Prof_Exp)
+admin.site.register(Education)
+admin.site.register(About_Me)
